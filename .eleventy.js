96910ae8d78e1d04d1798c324a222b4cf0de5964
Michelle <michelle.en4@gmail.com>
2023-05-14 16:36:25 -0400

    eleventy update + pathprefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const { DateTime } = require('luxon')
 const util = require('util')
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy')
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
+
 	eleventyConfig.addPassthroughCopy({ 'src/images': 'images' })
 
 	eleventyConfig.addFilter('formatDate', (dateObj) => {
